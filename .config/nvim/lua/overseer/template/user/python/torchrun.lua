5762edf07d8ad2e1a3d4d2bfb7fb041cc65a2817
Garrett Goon <goon@ibm.com>
2025-04-24 14:44:42 -0400

    overseer torch run read CUDA devices as default

--- .config/nvim/lua/overseer/template/user/python/torchrun.lua
+++ .config/nvim/lua/overseer/template/user/python/torchrun.lua
@@ -1,12 +1,23 @@
+local function get_visible_cuda_devices()
+	local devices = os.getenv("CUDA_VISIBLE_DEVICES")
+	if not devices or devices == "" then
+		return nil
+	end
+	return #(devices:gsub("[^,]", "")) + 1
+end
+
 return {
 	name = "torchrun",
 	builder = function()
 		local default_sh_full_path = vim.fn.expand("%:p")
 		local cmd = { "torchrun", "--nproc-per-node" }
 
-		local world_size = vim.fn.input("\n\nWORLD_SIZE (default 2):")
+		local num_gpus = get_visible_cuda_devices()
+		local default_world_size = num_gpus or 2
+		local world_size =
+			vim.fn.input("\n\nWORLD_SIZE (default " .. tostring(default_world_size) .. "):")
 		if world_size == "" then
-			world_size = "2"
+			world_size = tostring(default_world_size)
 		end
 
 		table.insert(cmd, world_size)
