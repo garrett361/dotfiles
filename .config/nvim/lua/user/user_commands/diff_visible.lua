0d60313a3b24ea987579cd3fa427913a6b1f7344
Garrett Goon <goon@ibm.com>
2025-07-11 10:19:35 -0400

    fix Diff --> D user command and add Do

--- .config/nvim/lua/user/user_commands/diff_visible.lua
+++ .config/nvim/lua/user/user_commands/diff_visible.lua
@@ -1,13 +1,20 @@
-vim.api.nvim_create_user_command("Diff", function(opts)
+vim.api.nvim_create_user_command("D", function(opts)
 	-- Get all currently visible windows
-	local windows = vim.api.nvim_list_wins()
+	-- local windows = vim.api.nvim_list_wins()
+	local buffers = require("nvim_utils").get_visible_buffers()
 	-- Apply difft to each visible window
-	for _, win in ipairs(windows) do
+	for _, buf in ipairs(buffers) do
 		-- Execute diffoff in the context of each window
-		vim.api.nvim_win_call(win, function()
+		vim.api.nvim_buf_call(buf, function()
 			vim.cmd("difft")
 		end)
 	end
 end, {
 	desc = "Diff all visible buffers",
+})
+
+vim.api.nvim_create_user_command("Do", function(opts)
+	vim.cmd("diffo!")
+end, {
+	desc = "Turn off diff",
 })
