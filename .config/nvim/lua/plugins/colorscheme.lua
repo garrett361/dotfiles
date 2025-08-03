47a15873bdca2f37f407a8041e9693dc01c4c751
Garrett Goon <goon@ibm.com>
2025-08-02 21:20:05 -0400

    update colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,32 @@
-local function config()
-	require("nvim_utils").prequire("bamboo").load()
+-- local function config()
+-- 	require("nvim_utils").prequire("bamboo").load()
+-- end
+-- return { "ribru17/bamboo.nvim", config = config, lazy = false, priority = 1000 }
+
+return {
+  "Skardyy/makurai-nvim",
+  config = function()
+    -- you don't have to call setup
+    require "makurai".setup({
+      transparent = false, -- removes the bg color
+      bordered = false, -- removes the bg color from floats/popups
+      increase_contrast = false, -- only changes the line number and active line number for now.
+    })
+    vim.cmd.colorscheme("makurai_dark")
 	-- Set diff colors after loading colorscheme, so that they're not overwritten.
 	require("user.options").set_diff_hl()
-end
-return { "ribru17/bamboo.nvim", config = config, lazy = false, priority = 1000 }
+  end
+}
+
+
+-- return {
+-- 	"github-main-user/lytmode.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("lytmode").setup()
+-- 		vim.cmd.colorscheme("lytmode")
+-- 	-- Set diff colors after loading colorscheme, so that they're not overwritten.
+-- 	require("user.options").set_diff_hl()
+-- 	end,
+-- }
