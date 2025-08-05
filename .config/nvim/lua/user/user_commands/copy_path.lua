6f657e00932af736b62d65d6feb330e865cc1cd0
Garrett Goon <goon@ibm.com>
2025-08-05 17:05:56 -0400

    CopyPathPy

--- .config/nvim/lua/user/user_commands/copy_path.lua
+++ .config/nvim/lua/user/user_commands/copy_path.lua
@@ -1,2 +1,3 @@
 -- Copy pwd path to system clipboard
 vim.api.nvim_create_user_command("CopyPath", 'let @+=expand("%")', {})
+vim.api.nvim_create_user_command("CopyPathPy", 'let @+=substitute(expand("%:r"), "/", ".", "g")', {})
