1baef423d8eea096823cf34a0334b408e92d6891
Garrett Goon <goon@ibm.com>
2025-10-13 11:20:49 -0400

    comment

--- .config/nvim/lua/user/user_commands/diff_visible.lua
+++ .config/nvim/lua/user/user_commands/diff_visible.lua
@@ -5,6 +5,7 @@ vim.api.nvim_create_user_command("D", function(opts)
     end
 	-- Get all currently visible windows
 	-- local windows = vim.api.nvim_list_wins()
+    -- NOTE: @goon - probably could have just done windo difft and windo diffo!
 	local buffers = require("nvim_utils").get_visible_buffers()
 	-- Apply difft to each visible window
 	for _, buf in ipairs(buffers) do
