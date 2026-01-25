017b9094ab8f445a5d232072756770493de491c9
Garrett Goon <goon@ibm.com>
2026-01-24 22:15:44 -0500

    fix: claude_global due to .claude name collision

--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@
 mkdir ~/.config
 is_linux=$(uname -s | grep -iq linux && echo 1 || echo 0)
 # Link all config and script files to their expected locations.
-for localdir in ".local" ".config" ".claude"; do
+for localdir in ".local" ".config"; do
 	dirpath=$(readlink -f $localdir)
 	ln -sfF $dirpath/* $HOME/$localdir
 done
+
+# symlink the global claude dir to ~/.claude. Need distinguishing name (claude_global) because `.claude/` in dotfiles repo is interpreted as repo-specific claude settings.
+dirpath=$(readlink -f $localdir)
+ln -sfF $(readlink -f claude_global)/* ${HOME}/.claude
 
 for localfile in ".commonrc" ".vimrc" ".bashrc" ".zshrc" ".gitignore_global" ".stylua.toml" ".rg" ".ipython/profile_default/ipython_config.py"; do
     filepath=$(readlink -f $localfile)
