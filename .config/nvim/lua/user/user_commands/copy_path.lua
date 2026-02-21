3edcb7cc1ecc6effaa754e1a94aa26e7eeaab9ea
Garrett Goon <goon@ibm.com>
2026-02-21 15:04:28 -0500

    copy path full

--- .config/nvim/lua/user/user_commands/copy_path.lua
+++ .config/nvim/lua/user/user_commands/copy_path.lua
@@ -1,3 +1,6 @@
--- Copy pwd path to system clipboard
-vim.api.nvim_create_user_command("CopyPath", 'let @+=expand("%")', {})
-vim.api.nvim_create_user_command("CopyPathPy", 'let @+=substitute(substitute(expand("%:r"), "/", ".", "g"), "^\\\\.", "", "")', {})
+-- Copy relative path to system clipboard
+vim.api.nvim_create_user_command("CopyPath", 'let @+=expand("%:.")', {})
+-- Copy full/absolute path to system clipboard
+vim.api.nvim_create_user_command("CopyPathFull", 'let @+=expand("%:p")', {})
+-- Copy relative path as Python module notation (e.g., foo/bar.py -> foo.bar)
+vim.api.nvim_create_user_command("CopyPathPy", 'let @+=substitute(substitute(expand("%:.:r"), "/", ".", "g"), "^\\\\.", "", "")', {})
