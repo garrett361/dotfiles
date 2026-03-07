467733df7a2893f285015e10c0f6c77f0e5e07fe
Garrett Goon <goon@ibm.com>
2026-03-06 22:04:36 -0500

    feat(nvim): add CopyPathFullLoc command with line number and range support

--- .config/nvim/lua/user/user_commands/copy_path.lua
+++ .config/nvim/lua/user/user_commands/copy_path.lua
@@ -4,3 +4,11 @@ vim.api.nvim_create_user_command("CopyPath", 'let @+=expand("%:.")', {})
 vim.api.nvim_create_user_command("CopyPathFull", 'let @+=expand("%:p")', {})
 -- Copy relative path as Python module notation (e.g., foo/bar.py -> foo.bar)
 vim.api.nvim_create_user_command("CopyPathPy", 'let @+=substitute(substitute(expand("%:.:r"), "/", ".", "g"), "^\\\\.", "", "")', {})
+-- Copy full/absolute path with line number or visual range (e.g., /abs/path:42 or /abs/path:10-20)
+vim.api.nvim_create_user_command("CopyPathFullLoc", function(opts)
+    local path = vim.fn.expand("%:p")
+    local result = opts.line1 == opts.line2
+        and path .. ":" .. opts.line1
+        or path .. ":" .. opts.line1 .. "-" .. opts.line2
+    vim.fn.setreg("+", result)
+end, { range = true })
