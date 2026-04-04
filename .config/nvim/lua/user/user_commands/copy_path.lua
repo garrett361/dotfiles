1a2840924c00ced78a333190e4025ceb9257142b
Garrett Goon <goon@ibm.com>
2026-04-03 20:35:12 -0400

    refactor(nvim): improve copy_path commands with visual range support
    Refactored copy_path user commands from string-based to function-based
    implementations. Extracted common logic into helper functions and added
    visual mode support for copying paths with line ranges.

--- .config/nvim/lua/user/user_commands/copy_path.lua
+++ .config/nvim/lua/user/user_commands/copy_path.lua
@@ -1,18 +1,46 @@
+local function copy_path(path_expr)
+	vim.fn.setreg("+", vim.fn.expand(path_expr))
+end
+
+local function copy_path_with_loc(path_expr)
+	local path = vim.fn.expand(path_expr)
+	local start_line = vim.fn.line("v")
+	local end_line = vim.fn.line(".")
+
+	if start_line > end_line then
+		start_line, end_line = end_line, start_line
+	end
+
+	local result = start_line == end_line and path .. ":" .. start_line
+		or path .. ":" .. start_line .. "-" .. end_line
+	vim.fn.setreg("+", result)
+end
+
 -- Copy relative path to system clipboard
-vim.api.nvim_create_user_command("CopyPath", 'let @+=expand("%:.")', {})
+vim.api.nvim_create_user_command("CopyPath", function()
+	copy_path("%:.")
+end, {})
 -- Copy full/absolute path to system clipboard
-vim.api.nvim_create_user_command("CopyPathFull", 'let @+=expand("%:p")', {})
+vim.api.nvim_create_user_command("CopyPathFull", function()
+	copy_path("%:p")
+end, {})
 -- Copy relative path as Python module notation (e.g., foo/bar.py -> foo.bar)
-vim.api.nvim_create_user_command("CopyPathPy", 'let @+=substitute(substitute(expand("%:.:r"), "/", ".", "g"), "^\\\\.", "", "")', {})
--- Copy full/absolute path with line number or visual range (e.g., /abs/path:42 or /abs/path:10-20)
-vim.api.nvim_create_user_command("CopyPathFullLoc", function(opts)
-    local path = vim.fn.expand("%:p")
-    local result = opts.line1 == opts.line2
-        and path .. ":" .. opts.line1
-        or path .. ":" .. opts.line1 .. "-" .. opts.line2
-    vim.fn.setreg("+", result)
-end, { range = true })
-vim.keymap.set({ "n", "v" }, "<leader>yl", ":CopyPathFullLoc<CR>", { desc = "Copy path:line to clipboard" })
-vim.keymap.set({ "n" }, "<leader>yy", ":CopyPath<CR>", { desc = "Copy path:line to clipboard" })
-vim.keymap.set({ "n" }, "<leader>yp", ":CopyPathPy<CR>", { desc = "Copy path:line to clipboard" })
-vim.keymap.set({ "n" }, "<leader>yf", ":CopyPathFull<CR>", { desc = "Copy path:line to clipboard" })
+vim.api.nvim_create_user_command(
+	"CopyPathPy",
+	'let @+=substitute(substitute(expand("%:.:r"), "/", ".", "g"), "^\\\\.", "", "")',
+	{}
+)
+
+vim.keymap.set("n", "<leader>yy", function()
+	copy_path("%:.")
+end, { desc = "Copy relative path to clipboard" })
+vim.keymap.set("x", "<leader>yy", function()
+	copy_path_with_loc("%:.")
+end, { desc = "Copy relative path with lines to clipboard" })
+vim.keymap.set("n", "<leader>yp", ":CopyPathPy<CR>", { desc = "Copy Python path to clipboard" })
+vim.keymap.set("n", "<leader>yf", function()
+	copy_path("%:p")
+end, { desc = "Copy absolute path to clipboard" })
+vim.keymap.set("x", "<leader>yf", function()
+	copy_path_with_loc("%:p")
+end, { desc = "Copy absolute path with lines to clipboard" })
